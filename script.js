3d71c96cf1fc42a57e3d4c3968647082a66f4757
ManiKandan2129 <137371444+ManiKandan2129@users.noreply.github.com>
2023-08-30 19:31:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var row = document.getElementById("row1");
 
 //fetch data from the api and display it on cards
 async function data(){
-    var data1 = await fetch("http://makeup-api.herokuapp.com/api/v1/products.json");
+    var data1 = await fetch("https://hp-api.onrender.com/api/characters");
     var data2 = await data1.json();
     for(let i=0; i< 100; i++){
         console.log(data2[i]);
@@ -14,12 +14,12 @@ async function data(){
         
         col.innerHTML = `
         <div class="card" style="width: 18rem;">
-            <img src = "${data2[i].image_link}" class="card-img-top" alt="No image" id="card-image">
+            <img src = "${data2[i].image}" class="card-img-top" alt="No image" id="card-image">
             <div class="card-body">
               <h5 class="card-title">${data2[i].name}</h5>
-              <h6 class="card-text">Category: ${data2[i].product_type}</h6>
-              <h6 class="price-tag">Price: ${data2[i].price} ${data2[i].price_sign}</h6>
-              <a href="${data2[i].product_link}" target = "_blank" class="btn btn-primary">Buy this</a>
+              <h6 class="card-text">House: ${data2[i].house}</h6>
+              <h6  class="card-text">Species: ${data2[i].species}</h6>
+              <h6  class="card-text">Actor: ${data2[i].actor}</h6>
             </div>
           </div>`
 }
